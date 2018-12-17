d9541025587433d45c1ced9eebdcfd61775addd5
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2018-12-17 14:01:26 +0200

    Adding PLA (Only works for 2 operand instructions)

--- pla.vhd
+++ pla.vhd
@@ -2,31 +2,43 @@ LIBRARY IEEE;
 USE IEEE.std_Logic_1164.all;
 
 ENTITY pla IS
-PORT(	IR : in std_logic_vector (15 downto 0) ;	
-   	output: out std_logic_vector (6 downto 0);
-	MPC : in std_logic_vector(6 downto 0));
+	PORT(	IR : in std_logic_vector (15 downto 0) ;	
+   		output: out std_logic_vector (6 downto 0);
+		MPC : in std_logic_vector(6 downto 0)
+	);
 END ENTITY pla;
 
 ARCHITECTURE Data_flow OF pla IS
 BEGIN 
--- if the MPC is at the start(after fetching the instruction ) and it's a two op instruction
--- then go to address of decoding first operand
-output <= "0000000" when MPC = "0000010"  and IR(3 downto 0) < "1000"
--- if we are not at the start ,but the op code is two operand and we decoded the first op
--- then go to address of decoding second operand
-else "0000000"  when  (MPC < "111011") and IR(3 downto 0) < "1000"
--- if we are two operand and done with two operands
--- then go to operation code
-else  "0000000"  when  (MPC > "111011") and IR(3 downto 0) < "1000"
--- if the MPC is at the start(after fetching the instruction ) and it's a 1 op instruction
--- then go to address of decoding first operand
-else "0000000" when MPC = "0000010"  and IR(4 downto 0) < "11101"
--- if we are not at the start ,but the op code is 1 operand and we decoded the first op
--- then go to address of operation
-else "0000000"  when  IR(4 downto 0) < "11101"
--- if the op code is branching ,then go to the branching operation
-else "0000000" when  IR(5 downto 0) < "1111110"
--- if the op code is no operation , then go to no op operation
-else "0000000" when  IR(6 downto 0) < "111111100";
+	process(IR, MPC)
+	begin
+		if(IR(3 downto 0) < "1001") then --If it's a two operand instruction
+			if(MPC = "0000011") then --If the MPC is at the the beginning of the ROM (Fetch first operand)
+				if(IR(15 downto 14) = "00") then       --Auto increment
+					output <= "0000100";
+				elsif(IR(15 downto 14) = "10") then    --Auto decrement
+					output <= "0001110";
+				elsif(IR(15 downto 14) = "01") then    --Indexed
+					output <= "0011101";
+				elsif(IR(15 downto 13) = "111") then   --Direct
+					output <= "0101111";
+				elsif(IR(15 downto 13) = "110") then   --Indirect
+					output <= "0110111";
+				end if;
+			else --If it's not, then it's a fetch the second operand
+				if(IR(9 downto 8) = "00") then       --Auto increment
+					output <= "1000100";
+				elsif(IR(9 downto 8) = "10") then    --Auto decrement
+					output <= "1001110";
+				elsif(IR(9 downto 8) = "01") then    --Indexed
+					output <= "1011101";
+				elsif(IR(9 downto 7) = "111") then   --Direct
+					output <= "1101111";
+				elsif(IR(9 downto 7) = "110") then   --Indirect
+					output <= "1110111";
+				end if;
+			end if;
+		end if;
+	end process;
 
 END ARCHITECTURE;
