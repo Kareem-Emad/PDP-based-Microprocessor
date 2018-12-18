1c3405da64e2dfa543b34b0da6f30694cfff44a4
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2018-12-18 22:14:07 +0200

    Fixed the mPC issue, done 2 tests (tests to continue)

--- ROMModule.vhd
+++ ROMModule.vhd
@@ -25,23 +25,6 @@ architecture ROMModuleWorkFlow of ROMModule is
 		);
 	end component;
 
-	component BitOring 
-		port(
-			IR         : in std_logic_vector(15 downto 0);
-			mPC        : in std_logic_vector(6 downto 0);
-			newMPC     : out std_logic_vector(6 downto 0)
-		);
-	end component;
-
-	component mPCUpdate
-		port(
-			PLAOut     : in std_logic_vector(6 downto 0);
-			branchType : in std_logic_vector(3 downto 0);
-			bitOringOut: in std_logic_vector(6 downto 0);
-			mPC        : out std_logic_vector(6 downto 0)
-		);
-	end component;
-		
 	component mPCInrementor
 		port(
 			input : in  std_logic_vector(6 downto 0);
@@ -51,21 +34,19 @@ architecture ROMModuleWorkFlow of ROMModule is
 
 	signal mPC           : std_logic_vector(6 downto 0);
 	signal ROMout        : std_logic_vector(19 downto 0);
-	signal BitOringOut   : std_logic_vector(6 downto 0);
 	signal PLAOut        : std_logic_vector(6 downto 0);
 	signal IncrementedmPC: std_logic_vector(6 downto 0);
 	
 begin
 	ROMX: ROM port map(mPC, ROMout);
 	
-	BitOringX: BitOring port map(IR, mPC, BitOringOut);
-
 	PLAX: PLA port map(IR, mPC, PLAOut);
 	
 	mPCInrementorX: mPCInrementor port map(mPC, IncrementedmPC);
 	
-	mPC <= PLAOut when (ROMout(10 downto 7) = "1001") and rising_edge(clk)
-	else BitOringOut when (ROMout(10 downto 7) = "1010") and rising_edge(clk) and (not (bitOringOut = mPC))
-	else IncrementedmPC when rising_edge(clk);
+	mPC <= PLAOut when (ROMout(10 downto 7) = "1001") and rising_edge(clk) 
+	else  mPC or "00000"&IR(8)&"0" when (ROMout(10 downto 7) = "1010") and rising_edge(clk) and mPC(6) = '1' and IR(8) = '1' 
+	else  mPC or "00000"&IR(2)&"0" when (ROMout(10 downto 7) = "1010") and rising_edge(clk) and mPC(6) = '0' and IR(2) = '1' 
+	else IncrementedmPC when rising_edge(clk) ;
 
 end architecture ROMModuleWorkFlow;
